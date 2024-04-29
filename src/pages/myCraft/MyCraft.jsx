b82f63367d31499cd127ff7216c73d4f7d86b115
Hadiur Rahman <hadiurahman139@gmail.com>
2024-04-29 19:23:54 +0600

    do the design part of the My Craft Page

--- src/pages/myCraft/MyCraft.jsx
+++ src/pages/myCraft/MyCraft.jsx
@@ -1,8 +1,93 @@
+import { useContext, useEffect, useState } from "react";
+import { ProjectContext } from "../../hooks/provider/ArtProvider";
+
+import "./myCraft.css";
+
 const MyCraft = () => {
+  const { user } = useContext(ProjectContext);
+  const { email: User_Email } = user || {};
+
+  const [myCrafts, setMyCrafts] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/addedSculptures/byEmail/${User_Email}`)
+      .then((res) => res.json())
+      .then((data) => setMyCrafts(data));
+  }, [User_Email]);
+
+  console.log(myCrafts);
+
   return (
-    <div>
-      <h1>this is it . this is my crafts</h1>
-    </div>
+    <section>
+      <div
+        className="hero h-[450px]"
+        style={{ backgroundImage: "url(/addcraft3.jpg)" }}
+      >
+        <div className="hero-overlay bg-opacity-40"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-xl ">
+            <h1 className="mb-5 text-4xl md:text-6xl font-dm text-white opacity-90 ">
+              {" "}
+              Your Exclusive Showcase
+            </h1>
+            <p className="mb-5 text-xl font-serif ">
+              Welcome to your Craftsman&apos;s Workshop, where creativity
+              thrives and control is at your fingertips. Showcase, edit, and
+              delete your craft creations with ease. This personalized studio
+              empowers you to curate and refine your portfolio, shaping your
+              artistic legacy with every stroke.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-16">
+        {myCrafts.map((item) => (
+          <div key={item._id} className="container">
+            <div className="content ">
+              <div>
+                <div className="content-overlay "></div>
+                <img
+                  className="content-image  h-full md:h-80"
+                  src={item.Image}
+                  alt="/404.jpg"
+                />
+                <div className="content-details fadeIn-bottom">
+                  <h3 className="content-title text-2xl font-serif">
+                    {item.Item_Name}
+                  </h3>
+                  <p className="content-text font-serif text-xl">
+                    <span className="font-semibold">Customization : </span>
+                    {item.Customization}
+                  </p>
+                  <div className="flex justify-center gap-5 my-0 md:my-2">
+                    <p className="content-text font-serif text-xl">
+                      <span className="font-semibold">Price : </span>
+                      {item.Price}
+                    </p>
+                    <p className="content-text font-serif text-xl">
+                      <span className="font-semibold">Rating : </span>
+                      {item.Rating}
+                    </p>
+                  </div>
+                  <p className="content-text font-serif text-xl">
+                    <span className="font-semibold">Stock Status : </span>
+                    {item.Stock_Status}
+                  </p>
+
+                  <button className="btn mt-0 md:mt-4 bg-[#628E90] text-white hover:bg-[#3C2317] transition-all">
+                    UPDATE
+                  </button>
+                  <button className="btn ml-4 bg-[#628E90] text-white hover:bg-[#3C2317] transition-all">
+                    DELETE
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
