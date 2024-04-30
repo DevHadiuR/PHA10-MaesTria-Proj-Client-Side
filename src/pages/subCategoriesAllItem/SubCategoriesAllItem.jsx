ab9ec9f847fdc5cefbc2bd848c524d2a83b48851
Hadiur Rahman <hadiurahman139@gmail.com>
2024-04-30 15:56:17 +0600

    challenge part 1 and 2 complete

--- src/pages/subCategoriesAllItem/SubCategoriesAllItem.jsx
+++ src/pages/subCategoriesAllItem/SubCategoriesAllItem.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const SubCategoriesAllItem = () => {
   const loaderData = useLoaderData();
@@ -18,18 +18,47 @@ const SubCategoriesAllItem = () => {
         within these remarkable works of art.
       </p>
 
+      <div className="mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {loaderData.map((item) => (
+          <div key={item._id} className="container">
+            <div className="content ">
+              <div>
+                <div className="content-overlay "></div>
+                <img
+                  className="content-image  h-full md:h-80"
+                  src={item.Image}
+                  alt="/404.jpg"
+                />
+                <div className="content-details fadeIn-bottom">
+                  <h3 className="content-title text-2xl md:text-3xl font-serif">
+                    {item.Item_Name}
+                  </h3>
+                  <p className="font-serif text-xl md:text-2xl ">
+                    <span className="font-semibold ">subcategory Name : </span>
+                    {item.Subcategory_Name}
+                  </p>
+                  <div className="flex justify-center items-center gap-5">
+                    <p className=" font-serif text-xl md:text-2xl">
+                      <span className="font-semibold">Price : </span>
+                      {item.Price}
+                    </p>
+                    <p className=" font-serif text-xl md:text-2xl">
+                      <span className="font-semibold">Rating : </span>
+                      {item.Rating}
+                    </p>
+                  </div>
 
-<div className="mt-16">
-
-{
-    loaderData.map(item=>
-    
-    
-    )
-}
-
-</div>
-
+                  <Link to={`/craftItemDetail/${item._id}`}>
+                    <button className="btn mt-2 md:mt-4 bg-[#628E90] text-white hover:bg-[#3C2317] transition-all">
+                      View Details
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
